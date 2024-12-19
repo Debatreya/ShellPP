7a03fe8303f0e74078a58594fd8bd28eb1bc33bb
Debatreyta <debatreyadas@gmail.com>
2024-12-19 10:39:37 +0530

    type implemented

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,111 @@
 #include <string>
 using namespace std;
 
-bool checkECHO(string input){
-  if(input.substr(0, 4) == "echo"){
+// // Declare an array of strings having valid commands (GLOBAL)
+string commands[] = {"echo", "type", "exit"};
+
+// Helper Functions
+// TRIM removes spaces from the beginning and end of the string
+string trim(string input)
+{
+  // Remove spaces from the beginning
+  while (input[0] == ' ')
+  {
+    input = input.substr(1);
+  }
+  // Remove spaces from the end
+  while (input[input.size() - 1] == ' ')
+  {
+    input = input.substr(0, input.size() - 1);
+  }
+  return input;
+}
+
+// Bool functions to check Validity of the command
+// checkECHO checks if the command is echo
+bool checkECHO(string input)
+{
+  // Check if the input starts with "echo"
+  if (input.substr(0, 4) == "echo")
+  {
+    return true;
+  }
+  return false;
+}
+// checkTYPE checks if the command is type
+bool checkTYPE(string input)
+{
+  // Check if the input starts with "type"
+  if (input.substr(0, 4) == "type")
+  {
     return true;
   }
   return false;
 }
 
-string ECHO(string input){
-  string res = input.substr(5);
+// Extract and Execute the command
+// ECHO extracts the string after "echo" and returns it
+string ECHO(string input)
+{
+  string res = input.substr(5); // Remove "echo" from the input
+  // Trim spaces from the beginning and end of the string
+  res = trim(res);
   return res;
 }
+// TYPE extracts the string after "type" and returns it
+string TYPE(string input)
+{
+  string res = input.substr(5); // Remove "type" from the input
+  // Trim spaces from the beginning and end of the string
+  res = trim(res);
+  // Check if the command is valid
+  bool valid = false;
+  for (int i = 0; i < 3; i++)
+  {
+    if (res == commands[i])
+    {
+      valid = true;
+      break;
+    }
+  }
+  // If the command is valid, print it
+  if (valid)
+  {
+    return res + " is a shell builtin";
+  }
+  return res + ": not found";
+}
 
-int main() {
+int main()
+{
   // Flush after every cout / std:cerr
   cout << unitbuf;
   cerr << unitbuf;
-
-  while (true) {
+  // Main loop to take input and execute commands (REPL) // Read-Eval-Print-Loop
+  while (true)
+  {
     cout << "$ ";
     string input;
     getline(cin, input);
-    if (input == "exit 0") {
+    // Trim spaces from the beginning and end of the string
+    input = trim(input);
+    // Check and Execute the commands
+    if (input == "exit 0")
+    {
       return 0;
-    } else if(checkECHO(input)){
+    }
+    else if (checkECHO(input))
+    {
       string res = ECHO(input);
       cout << res << endl;
-    }else {
+    }
+    else if (checkTYPE(input))
+    {
+      string res = TYPE(input);
+      cout << res << endl;
+    }
+    else
+    {
       cerr << input << ": command not found" << endl;
     }
   }
